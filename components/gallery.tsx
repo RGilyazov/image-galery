ba77adf329e4c97e8a05bf075b15f9bfb1d2b1f7
bbOracul <GilyazovRuslan@Gmail.com>
2022-08-22 17:45:33 +0500

    marging and padding

--- components/gallery.tsx
+++ components/gallery.tsx
@@ -8,11 +8,14 @@ export default function Gallery({ name, description, images }: GalleryData) {
       <Head>
         <title>{name}</title>
       </Head>
-      <div className="text-left flex-wrap border-2 rounded-lg mt-1 bg-gray-50">
+      <div className="text-left flex-wrap border-2 rounded-lg mt-1 mb-1 bg-gray-50">
         <h1 className="text-lg font-bold mt-2 mb-2 ml-2 ">{name}</h1>
         <h2 className="text-sm text-gray-500 mt-2 mb-2 ml-2 ">
           {images.length} images in this gallery
         </h2>
+      </div>
+      <div className="text-left flex-wrap border-2 rounded-lg mt-1 mb-2 bg-gray-50">
+        <h2 className="text-sm text-gray-500 mt-2 mb-2 ml-2 ">{description}</h2>
       </div>
       <ul className="flex-grow flex flex-row justify-evenly flex-wrap border-2 rounded-lg gap-2 p-2 bg-gray-50">
         {images.map((imageData: ImageData) => (
@@ -26,9 +29,6 @@ export default function Gallery({ name, description, images }: GalleryData) {
           </li>
         ))}
       </ul>
-      <div className="text-left flex-wrap border-2 rounded-lg mt-1 bg-gray-50">
-        <h2 className="text-sm text-gray-500 mt-2 mb-2 ml-2 ">{description}</h2>
-      </div>
     </>
   );
   return result;
